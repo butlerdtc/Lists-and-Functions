acae14f048d27840a787c9dcb9caac598caa5d75
butlerdtc <butlerr2@middleton.school.nz>
2024-02-16 11:29:46 +1300

    Final Update

--- Silent Auction/silent_auction.py
+++ Silent Auction/silent_auction.py
@@ -21,8 +21,8 @@ def formatter(symbol, text):
 
 def menu():
     print(formatter("*", "Robson's Silent Auction"))
-    item()
-    reserve()
+    bidding()
+    print("Goodbye")
 
 
 def item():
@@ -31,9 +31,30 @@ def item():
 
 
 def reserve():
-    price = number_checker(float(input("What would you like the reserve "
-                                       "price to be: ")))
+    price = number_checker("What would you like the reserve price to be: ")
     return price
+
+
+def bidding():
+    auction_item = item()
+    cost = reserve()
+    bid = 0
+    print("The auction has started!")
+    print("Please enter '-1' to exit the auction")
+    print()
+    while True:
+        print(f"Highest bid currently is {bid}")
+        new_bid = float(input("What is your bid: "))
+        if new_bid < 0:
+            break
+        elif new_bid > bid:
+            bid = new_bid
+        else:
+            print("Please enter higher bid")
+    if bid >= cost:
+        print(f"The {auction_item} sold for {bid}")
+    else:
+        print(f"The {auction_item} did not sell")
 
 
 # Main Routine
